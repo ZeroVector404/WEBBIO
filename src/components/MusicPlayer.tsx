d1f3c9f97bc837f0a80b6ebc505f34e0b31cc742
Spoxer12 <vpnreal253@gmail.com>
2026-02-01 12:58:56 +0700

    add-photo-g-song

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -5,6 +5,7 @@ import { Play, Pause, SkipBack, SkipForward, Volume2, VolumeX, Volume1, Repeat }
 // === Add your songs here ===
 // Use full YouTube URLs (e.g. https://www.youtube.com/watch?v=... or https://youtu.be/...)
 const songs = [
+  { title: "Photograph", artist: "Ed Sheeran", url: "https://youtu.be/SlbfAYvA_gI?si=CgJrT4EkLpaUnQhy", banner: "https://www.siamzone.com/music/yt/KKQl-pIRQMY.jpg" },
   { title: "Falling", artist: "Harry Styles", url: "https://youtu.be/olGSAVOkkTI?si=c0EY0wiHGqlzPoK3", banner: "https://i.ytimg.com/vi/olGSAVOkkTI/maxresdefault.jpg" },
   { title: "About You", artist: "The 1975", url: "https://youtu.be/tGv7CUutzqU?si=pvAtfYuLimWzBDSW", banner: "https://i.ytimg.com/vi/tGv7CUutzqU/hq720.jpg" },
   { title: "A Moment", artist: "ZWEED N' ROLL", url: "https://youtu.be/7grezzVb-AU?si=DRafqKaNMi0OJY9D", banner: "https://i.scdn.co/image/ab67616d0000b2731e228e68d82866f9a2a19265" },
